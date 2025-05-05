a45382434e327ee777ff8a8c237a6a00d94f6ecf
Ehiane <e.oigiagbe@wsu.edu>
2025-05-05 09:20:10 -0700

    configured the scss and databse in app.py

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 # My app
 app = Flask(__name__) # acts like the hub/manager to all of the different pages.
+Scss(app) # allows us to use scss files in our templates folder
+
+# database setup
+# sqlalchemy is an ORM (Object Relational Mapper) that allows us to interact with the database using Python objects instead of SQL queries.
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db" # path to the database file
+db = SQLAlchemy(app) # allows us to use the database in our app
 
 
 # this decorator allows you to route to the homepage ("/") --> root directory
