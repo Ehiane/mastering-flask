65a890b2d25cb1a995660d933e5ba0e2bc07b71b
Ehiane <e.oigiagbe@wsu.edu>
2025-05-05 08:52:11 -0700

    basic linking to the index.html with Jinja

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # imports 
-from flask import Flask
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 # My app
-app = Flask(__name__)
+app = Flask(__name__) # acts like the hub/manager to all of the different pages.
 
 
 # this decorator allows you to route to the homepage ("/") --> root directory
 @app.route("/")
 def index():
     """First main page of our application"""
-    return "Testing 123"
+    return render_template("index.html") # access the index.html file from the templates folder
 
 if __name__ in "__main__":
     app.run(debug=True)
